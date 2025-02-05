902692ffebda3fc49c6f2a83c920aed897fb5022
John Mason <johnmsn22@gmail.com>
2025-02-05 14:39:14 +0100

    add text

--- src/components/shared/ComingSoon.tsx
+++ src/components/shared/ComingSoon.tsx
@@ -8,6 +8,8 @@ const ComingSoon = () => {
         <div className="bg-white w-full max-w-3xl mx-auto flex flex-col items-center shadow-lg rounded-xl py-16">
           <ConstructionIcon size={58} />
           <h1 className="font-bold text-4xl mt-8 uppercase text-center">Site Under Construction</h1>
+
+          <p className="pt-8">Check back soon...</p>
         </div>
       </div>
     </div>
