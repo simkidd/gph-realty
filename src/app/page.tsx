09fe7514071068415385d082f980e990b32411a0
John Mason <johnmsn22@gmail.com>
2025-02-05 14:19:38 +0100

    construction page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
-import Testimonials from '@/components/home/Testimonials'
+// import Testimonials from '@/components/home/Testimonials'
+
+import ComingSoon from "@/components/shared/ComingSoon"
 
 const Homepage = () => {
   return (
-    <div className='py-[90px]'>
-      <div>
-        <Testimonials />
-      </div>
-    </div>
+    // <div className='py-[90px]'>
+    //   <div>
+    //     <Testimonials />
+    //   </div>
+    // </div>
+    <ComingSoon />
   )
 }
 
