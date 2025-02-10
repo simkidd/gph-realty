9dff48b997b88e2855e890ada505f9a09101900b
John Mason <johnmsn22@gmail.com>
2025-02-10 15:57:55 +0100

    map height

--- src/components/contact/ContactContent.tsx
+++ src/components/contact/ContactContent.tsx
@@ -79,12 +79,12 @@ const ContactContent = () => {
 
             {/* map */}
             <div className="p-[30px] shadow-custom bg-white mt-[45px]">
-              <div className="h-[250px]">
-                <div>
+              <div className="h-[250px] w-full">
+                <div className="h-full">
                   <iframe
                     src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d557.8187122340947!2d6.993123192947521!3d4.824900415175618!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sng!4v1719912936935!5m2!1sen!2sng"
                     width="100%"
-                    height="300"
+                    height="100%"
                     style={{ border: 0 }}
                     allowFullScreen={false}
                     loading="lazy"
