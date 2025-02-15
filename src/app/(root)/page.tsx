9066ebe362e94e54e7e888f095b02b276b70ceb4
John Mason <johnmsn22@gmail.com>
2025-02-15 07:56:33 +0100

    cta section

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,16 +1,18 @@
 import BrowseOffers from "@/components/home/BrowseOffers";
+import Cta from "@/components/home/Cta";
 import HeroSection from "@/components/home/Hero";
 import Testimonials from "@/components/home/Testimonials";
 import WhyUs from "@/components/home/WhyUs";
 
 const Homepage = () => {
   return (
-    <div className="py-[90px]">
+    <div className="">
       <div>
         <HeroSection />
         <WhyUs />
         <BrowseOffers />
         <Testimonials />
+        <Cta />
       </div>
     </div>
   );
