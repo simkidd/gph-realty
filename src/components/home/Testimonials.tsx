86c2881f05f37e6f1cca359413cc9cdd2ab2fc41
John Mason <johnmsn22@gmail.com>
2025-02-15 11:53:28 +0100

    add review bg

--- src/components/home/Testimonials.tsx
+++ src/components/home/Testimonials.tsx
@@ -1,17 +1,22 @@
-import React from "react";
-import Badge from "../ui/Badge";
 import Reviews from "./Reviews";
 
 const Testimonials = () => {
   return (
-    <section className="bg-black py-[90px]">
-      <div className="container mx-auto px-3">
-        <div className="pb-[50px] text-white flex flex-col items-center group">
-          <Badge className="rounded-[5px] font-medium capitalize bg-primary text-white">Our</Badge>
-          <h2 className="pt-[15px] leading-[1.3] text-[30px] ">
-            Happy clients
-          </h2>
-          <hr className="w-[80px] h-[2px] text-white group-hover:w-[150px] transition-all duration-500 my-5" />
+    <section className="relative bg-review-bg bg-no-repeat bg-cover bg-center py-[90px]">
+      <div className="bg-black/70 w-full h-full absolute top-0 left-0"></div>
+      <div className="container mx-auto px-3 relative">
+        <div className="text-center pb-[53px]">
+          <div className="pb-3">
+            <h6 className="font-medium text-[13px] uppercase text-primary ">
+              Our
+            </h6>
+          </div>
+          <div className="text-center flex flex-col items-center group">
+            <h2 className="leading-[1.3] text-[30px] text-white font-bold">
+              Happy clients
+            </h2>
+            <hr className="w-[80px] h-[2px] text-white group-hover:w-[150px] transition-all duration-500 my-5" />
+          </div>
         </div>
         <div>
           <Reviews />
