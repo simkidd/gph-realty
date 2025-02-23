50882dc94c814c6a341c9dd5ee7d38319b89bd95
John Mason <johnmsn22@gmail.com>
2025-02-23 08:15:20 +0100

    added NEXT_PUBLIC_API_URL

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
 
 const instance = axios.create({
   baseURL: BASE_URL,
