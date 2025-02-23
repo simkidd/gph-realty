661935de7c14bcdd5f5e370dcad6dc0065daa5ba
John Mason <johnmsn22@gmail.com>
2025-02-23 15:52:18 +0100

    metadata.ts

--- src/app/(root)/listing/property/[slug]/page.tsx
+++ src/app/(root)/listing/property/[slug]/page.tsx
@@ -1,7 +1,5 @@
 import ClientPropertyDetails from "@/components/property/ClientPropertyDetails";
-import { IProperty } from "@/interfaces/property.interface";
-import { getPropertyBySlug } from "@/lib/api/properties";
-import { config } from "@/utils/config";
+import { getPropertyMetadata } from "./metadata";
 
 export const generateMetadata = async ({
   params,
@@ -9,32 +7,7 @@ export const generateMetadata = async ({
   params: Promise<{ slug: string }>;
 }) => {
   const { slug } = await params;
-  const property: IProperty = await getPropertyBySlug(slug);
-
-  return {
-    title: property.name,
-    description: property.description,
-    canonical: `${config.SITE_URL}/listing/property/${property.slug}`,
-    openGraph: {
-      type: "website",
-      url: `${config.SITE_URL}/listing/property/${property.slug}`,
-      title: property.name,
-      description: property.description,
-      images: [
-        {
-          url: property.images[0].imageUrl,
-          width: 1200,
-          height: 630,
-        },
-      ],
-    },
-    twitter: {
-      cardType: "summary_large_image",
-      title: property.name,
-      description: property.description,
-      image: property.images[0].imageUrl,
-    },
-  };
+  return await getPropertyMetadata(slug);
 };
 
 
