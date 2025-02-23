5df5d3e5cc0360b2771266981fb1e94bb02f0893
John Mason <johnmsn22@gmail.com>
2025-02-23 11:29:31 +0100

    remove select

--- src/lib/properties.ts
+++ src/lib/properties.ts
@@ -1,11 +1,5 @@
 import { prisma } from "./prisma";
 
 export async function getProperties() {
-  return await prisma.property.findMany({
-    select: {
-      id: true,
-      name: true,
-      slug: true,
-    },
-  });
+  return await prisma.property.findMany({});
 }
